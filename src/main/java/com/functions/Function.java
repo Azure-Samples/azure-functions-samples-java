1b45d9f8911a1557ae2aa401ccb65b7d65d1445a
Shreyas Gopalakrishna <shreyasg@microsoft.com>
2022-01-21 15:57:02 -0600

    Added java examples for the usage of different triggers

--- src/main/java/com/functions/Function.java
+++ src/main/java/com/functions/Function.java
@@ -12,6 +12,7 @@ import com.microsoft.azure.functions.HttpRequestMessage;
 import com.microsoft.azure.functions.HttpResponseMessage;
 import com.microsoft.azure.functions.HttpStatus;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FixedDelayRetry;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
 
@@ -45,5 +46,61 @@ public class Function {
         } else {
             return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
         }
+    }
+
+    public static int count = 1;
+
+    /**
+     * This function listens at endpoint "/api/HttpExampleRetry". The function is re-executed in case of errors until the maximum number of retries occur.
+     * Retry policies: https://docs.microsoft.com/en-us/azure/azure-functions/functions-bindings-error-pages?tabs=java
+     */
+    @FunctionName("HttpExampleRetry")
+    @FixedDelayRetry(maxRetryCount = 3, delayInterval = "00:00:05")
+    public HttpResponseMessage HttpExampleRetry(
+        @HttpTrigger(
+            name = "req",
+            methods = {HttpMethod.GET, HttpMethod.POST},
+            authLevel = AuthorizationLevel.ANONYMOUS)
+            HttpRequestMessage<Optional<String>> request,
+        final ExecutionContext context) throws Exception {
+        context.getLogger().info("Java HTTP trigger processed a request.");
+
+        if(count<3) {
+            count ++;
+            throw new Exception("error");
+        }
+
+        // Parse query parameter
+        final String query = request.getQueryParameters().get("name");
+        final String name = request.getBody().orElse(query);
+
+        if (name == null) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
+        } else {
+            return request.createResponseBuilder(HttpStatus.OK).body(name).build();
+        }
+    }
+
+    /**
+     * This function listens at endpoint "/api/HttpTriggerJavaVersion".
+     * It can be used to verify the Java home and java version currently in use in your Azure function
+     */
+    @FunctionName("HttpTriggerJavaVersion")
+    public static HttpResponseMessage HttpTriggerJavaVersion(
+        @HttpTrigger(
+            name = "req",
+            methods = {HttpMethod.GET, HttpMethod.POST},
+            authLevel = AuthorizationLevel.ANONYMOUS)
+            HttpRequestMessage<Optional<String>> request,
+        final ExecutionContext context
+    ) {
+        context.getLogger().info("Java HTTP trigger processed a request.");
+        final String javaVersion = getJavaVersion();
+        context.getLogger().info("Function - HttpTriggerJavaVersion" + javaVersion);
+        return request.createResponseBuilder(HttpStatus.OK).body("HttpTriggerJavaVersion").build();
+    }
+
+    public static String getJavaVersion() {
+        return String.join(" - ", System.getProperty("java.home"), System.getProperty("java.version"));
     }
 }
